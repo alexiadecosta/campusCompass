d503f7decaba5bb0cabb31111f4c48111c77d1f8
alexiadecosta <decostaalexia@gmail.com>
2026-05-29 15:52:07 -0400

    fix init configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,7 +5,8 @@ from flask_sqlalchemy import SQLAlchemy #object-relational mapper
 db=SQLAlchemy() #database
 def create_app():
   app=Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI']=\'postgresql://postgres:password@localhost/campusCompass'
+  app.config['SQLALCHEMY_DATABASE_URI']=\
+    'postgresql://postgres:password@localhost/campusCompass'
   CORS(app)
   db.init_app(app)
   from app.routes import main
