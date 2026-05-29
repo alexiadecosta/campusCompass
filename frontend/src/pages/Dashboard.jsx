adc9e8fa01c40ec511e45af59ee12ec7c296b4f9
alexiadecosta <decostaalexia@gmail.com>
2026-05-29 16:22:29 -0400

    fixed formatting in dashboard code

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,19 +1,20 @@
 import React, {useEffect, useState} from "react";
 import api from "../api";
 
-function Dashboard(){
-  const[resources, setResources]=useState([]);
-  useEffect(()=>{
+function Dashboard() {
+  const [resources, setResources] = useState([]);
+  useEffect(() => {
     api.get("/recommendations").then(res => setResources(res.data));
   }, []);
   return (
     <div>
       <h1> Your Recommendations </h1>
       {resources.map((resource,index) => (
-      <div> key={index}>
+      <div key={index}>
         <h3> {resource.title} </h3>
         <p> {resource.category} </p>
       </div>
+      ))}
     </div>
   );
 }
